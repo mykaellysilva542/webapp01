e84cefa0b80b640be8179e273ff7c0cee3966a43
mykaellysilva542 <116382545+mykaellysilva542@users.noreply.github.com>
2022-10-22 11:16:08 -0300

    Update webapp01.py

--- webapp01.py
+++ webapp01.py
@@ -3,7 +3,7 @@
 import streamlit as st
 from PIL import Image
 
-image01 = Image.open('desenvolvimento.jpg')
+image01 = Image.open('maxresdefault.jpg')
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
 st.title("Bem vindo, ao como desenhar melhor 2.0!")
 
